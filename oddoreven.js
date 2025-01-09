cb8f42cef85a4ffc93c3606bac5f896760cab270
humairanusrath <humairanusrath7@gmail.com>
2025-01-09 19:52:09 +0530

    initial commit

---
+++ oddoreven.js
@@ -0,0 +1,12 @@
+class Solution {
+
+    isEven(n) {
+        let obj = new Solution();
+        if (n%2 === 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
