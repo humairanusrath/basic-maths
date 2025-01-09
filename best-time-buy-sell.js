82e32636fd4c756dc935b99efb3826c0f049f84a
humairanusrath <humairanusrath7@gmail.com>
2025-01-09 10:19:41 +0530

    initial commit

--- best-time-buy-sell.js
+++ best-time-buy-sell.js
@@ -3,11 +3,12 @@ function maxProfit(prices){
     prices[0] = 0;
     let profit = 0;
     for (let i=1; i<prices.length; i++){
-        if (buy >prices[i]){
-            buy = prices[i];
-            prices[i] =0;
+        if (buy >prices[i]){ //If the current day's price (prices[i]) is lower than the current buy price
+
+            buy = prices[i]; //this updates the buy price to the current day's lower price.
         } else {
-            profit = Math.max(profit[i] -buy, profit);
+            profit = Math.max(profit[i] -buy, profit); //prices[i] - buy calculates the profit if you sold the stock on the i-th day after buying at the buy price.
+            // Math.max() ensures that profit is updated to the maximum possible profit.
         }
     }
 return profit;
